ac991dc8310309f4d86105ecf81852f721ba2dc2
HeshLakshitha <lakshithapeiris6@gmail.com>
2019-02-27 22:08:53 -0800

    #13 Create gallery.php

---
+++ gallery.php
@@ -0,0 +1,178 @@
+<?php ?>
+<!DOCTYPE html>
+<html lang="en">
+
+
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>About Us || Tours-at-srilanka</title>
+        <!-- favicon -->
+        <link rel="shortcut icon" href="images/favicon.png" type="image/x-icon">
+
+        <link href="css/fontawesome.min.css" rel="stylesheet" type="text/css"/>
+        <link href="css/flaticon.css" rel="stylesheet" type="text/css"/>
+        <link href="css/bootstrap.min.css" rel="stylesheet" type="text/css"/>
+        <link href="css/owl.carousel.min.css" rel="stylesheet" type="text/css"/>
+        <link href="css/lightcase.css" rel="stylesheet" type="text/css"/>
+        <link href="css/style.css" rel="stylesheet" type="text/css"/>
+        <link href="css/responsive.css" rel="stylesheet" type="text/css"/>
+        <link href="css/custom.css" rel="stylesheet" type="text/css"/>
+        <link rel="stylesheet" type="text/css" href="css-1/fullwidth.css" media="screen" />
+        <link rel="stylesheet" type="text/css" href="rs-plugin/css/settings.css" media="screen" />
+        <link rel="stylesheet" href="css-1/rev-settings.css" type="text/css">
+        <!--<link href="fontawesome/css/all.css" rel="stylesheet" type="text/css"/>-->
+        <link href="font-awesome-4.7.0/css/font-awesome.css" rel="stylesheet" type="text/css"/>
+        <link href="font-awesome-4.7.0/css/font-awesome.min.css" rel="stylesheet" type="text/css"/>
+    </head>
+
+    <body>
+
+        <!-- preloader start -->
+
+        <!-- preloader end -->
+
+        <!--  header-section start  -->
+        <?php include './header.php'; ?>
+        <!--  header-section end  -->
+
+        <section class="single-banner about-banner">
+            <div class="page-breadcums">
+                <div class="container">
+                    <ul class="page-list">
+                        <li><a href="index.php">Home</a></li>
+                        <li>Gallery</li>
+                    </ul>
+                </div>
+            </div>
+            <div class="banner-content-area">
+                <div class="container">
+                    <div class="row justify-content-center">
+                        <div class="col-lg-10">
+                            <div class="banner-content text-center">
+                                <h1 class="banner-title">photo gallery</h1>
+                                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's<br/> standard dummy text ever since the 1500s.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <!-- events-gallery-section start -->
+        <section class="events-gallery-section section-padding">
+            <div class="container">
+                <div class="section-wrapper">
+                    <div class="event-gallery-slider owl-carousel grid">
+                        <div class="gallery-single-slide academy">
+                            <div class="event-gallery-thumb">
+                                <img class="gallery-popup" src="images/event-gallery/7.jpg" alt="event-gallery">
+                            </div>
+                            <div class="event-gallery-thumb">
+                                <img src="images/event-gallery/8.jpg" alt="event-gallery">
+                            </div>
+                            <div class="event-gallery-thumb">
+                                <img src="images/event-gallery/10.jpg" alt="event-gallery">
+                            </div>
+                        </div><!-- gallery-single-slide end -->
+                        <div class="gallery-single-slide">
+                            <div class="event-gallery-thumb">
+                                <img src="images/event-gallery/2.jpg" alt="event-gallery">
+                            </div>
+                            <div class="event-gallery-thumb">
+                                <img src="images/event-gallery/5.jpg" alt="event-gallery">
+                            </div>
+                            <div class="event-gallery-thumb">
+                                <img src="images/event-gallery/11.jpg" alt="event-gallery">
+                            </div>
+                        </div><!-- gallery-single-slide end -->
+                        <div class="gallery-single-slide">
+                            <div class="event-gallery-thumb">
+                                <img src="images/event-gallery/3.jpg" alt="event-gallery">
+                            </div>
+                            <div class="event-gallery-thumb">
+                                <img src="images/event-gallery/9.jpg" alt="event-gallery">
+                            </div>
+                            <div class="event-gallery-thumb">
+                                <img src="images/event-gallery/12.jpg" alt="event-gallery">
+                            </div>
+                        </div><!-- gallery-single-slide end -->
+                        <div class="gallery-single-slide">
+                            <div class="event-gallery-thumb">
+                                <img src="images/event-gallery/1.jpg" alt="event-gallery">
+                            </div>
+                            <div class="event-gallery-thumb">
+                                <img src="images/event-gallery/2.jpg" alt="event-gallery">
+                            </div>
+                            <div class="event-gallery-thumb">
+                                <img src="images/event-gallery/4.jpg" alt="event-gallery">
+                            </div>
+                        </div><!-- gallery-single-slide end -->
+                        <div class="gallery-single-slide academy">
+                            <div class="event-gallery-thumb grid-item">
+                                <img src="images/event-gallery/1.jpg" alt="event-gallery">
+                            </div>
+                            <div class="event-gallery-thumb">
+                                <img src="images/event-gallery/4.jpg" alt="event-gallery">
+                            </div>
+                            <div class="event-gallery-thumb">
+                                <img src="images/event-gallery/6.jpg" alt="event-gallery">
+                            </div>
+                        </div><!-- gallery-single-slide end -->
+                        <div class="gallery-single-slide">
+                            <div class="event-gallery-thumb">
+                                <img src="images/event-gallery/9.jpg" alt="event-gallery">
+                            </div>
+                            <div class="event-gallery-thumb grid-item">
+                                <img src="images/event-gallery/11.jpg" alt="event-gallery">
+                            </div>
+                            <div class="event-gallery-thumb">
+                                <img src="images/event-gallery/3.jpg" alt="event-gallery">
+                            </div>
+                        </div><!-- gallery-single-slide end -->
+                    </div>
+                </div>
+            </div>
+        </section>
+        <!-- events-gallery-section end -->
+
+        <!-- footer-section start -->
+        <?php include './footer.php'; ?>
+        <!-- footer-section end -->
+
+        <!-- scroll-to-top start -->
+        <div class="scroll-to-top">
+            <span class="scroll-icon">
+                <i class="fa fa-rocket" aria-hidden="true"></i>
+            </span>
+        </div>
+        <!-- scroll-to-top end -->
+
+
+        <script src="js/jquery-3.3.1.min.js"></script>
+        <!-- bootstrap js link -->
+        <script src="js/bootstrap.bundle.js"></script>
+        <script src="js/bootstrap.min.js"></script>
+        <!-- owl carousel js link -->
+        <script src="js/owl.carousel.min.js"></script>
+        <!-- lightcase js link -->
+        <script src="js/lightcase.js"></script>
+        <!-- waypoints js link -->
+        <script src="js/jquery.waypoints.min.js"></script>
+        <!-- countup js link -->
+        <script src="js/jquery.countup.min.js"></script>
+        <!-- circle-progress js link -->
+        <script src="js/circle-progress.min.js"></script>
+        <!-- countdown js link -->
+        <script src="js/jquery.countdown.js"></script>
+        <!-- goolg-map-activate js link -->
+        <script src="js/goolg-map-activate.js"></script>
+        <!-- main js link -->
+        <script src="js/main.js"></script>
+
+        <!-- Magnific popup js -->
+        <script src="js/jquery.magnific-popup.min.js"></script>
+    </body>
+
+</html>
