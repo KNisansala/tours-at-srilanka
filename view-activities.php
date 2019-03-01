dbfda62b2d2898d326f6ac68d5bc735619df83ac
HeshLakshitha <lakshithapeiris6@gmail.com>
2019-02-28 20:48:42 -0800

    #24 Create activities.php

---
+++ view-activities.php
@@ -0,0 +1,180 @@
+<?php ?>
+<!DOCTYPE html>
+<html lang="en">
+
+
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>About Us || Tours-at-srilanka</title>
+        <!-- favicon -->
+        <link rel="shortcut icon" href="images/favicon.png" type="image/x-icon">
+
+        <link href="css/fontawesome.min.css" rel="stylesheet" type="text/css"/>
+        <link href="css/flaticon.css" rel="stylesheet" type="text/css"/>
+        <link href="css/bootstrap.min.css" rel="stylesheet" type="text/css"/>
+        <link href="css/owl.carousel.min.css" rel="stylesheet" type="text/css"/>
+        <link href="css/lightcase.css" rel="stylesheet" type="text/css"/>
+        <link href="css/style.css" rel="stylesheet" type="text/css"/>
+        <link href="css/responsive.css" rel="stylesheet" type="text/css"/>
+        <link href="css/custom.css" rel="stylesheet" type="text/css"/>
+        <link rel="stylesheet" type="text/css" href="css-1/fullwidth.css" media="screen" />
+        <link rel="stylesheet" type="text/css" href="rs-plugin/css/settings.css" media="screen" />
+        <link rel="stylesheet" href="css-1/rev-settings.css" type="text/css">
+        <!--<link href="fontawesome/css/all.css" rel="stylesheet" type="text/css"/>-->
+        <link href="font-awesome-4.7.0/css/font-awesome.css" rel="stylesheet" type="text/css"/>
+    </head>
+
+    <body>
+
+        <!-- preloader start -->
+
+        <!-- preloader end -->
+
+        <!--  header-section start  -->
+        <?php include './header.php'; ?>
+        <!--  header-section end  -->
+
+        <section class="single-banner about-banner">
+            <div class="page-breadcums">
+                <div class="container">
+                    <ul class="page-list">
+                        <li><a href="index.php">Home</a></li>
+                        <li>Attraction View</li>
+                    </ul>
+                </div>
+            </div>
+            <div class="banner-content-area">
+                <div class="container">
+                    <div class="row justify-content-center">
+                        <div class="col-lg-10">
+                            <div class="banner-content text-center">
+                                <h1 class="banner-title">Attraction View</h1>
+                                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's<br/> standard dummy text ever since the 1500s.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <!-- blog-grid-section start -->
+        <section class="single-blog-section section-padding section-bg">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-8">
+                        <div class="entry-single">
+                            <div class="entry-single-thumb">
+                                <img src="assets/images/blog/b1.jpg" alt="single-post-image">
+                            </div>
+                            <div class="entry-single-content">
+                                <h3 class="entry-single-title">Malesuada Sociosqu Vel Iaculis Convallis Molestie Vel Sit, Nulla Sapien Eleifend.</h3>
+                                <p>Lorem Ipsum is simply dummy text of the printing and typesettig industry. Lorem Ipsum has been on the industry's standard dummy text unknown printer took a galley of type and scrambled it to make a type specimen book. Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+                                <p>Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown the  printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting.</p>
+                                <p>Remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets on containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum. </p>
+                            </div>
+                        </div>
+                    </div><!-- blog-list-wrapper end -->
+                    <div class="col-lg-4">
+                        <div class="sidebar-area">
+
+                            <div class="widget">
+                                <h5 class="widget-title">recent blogs</h5>
+                                <ul class="small-post-list">
+                                    <li class="post-item d-flex align-items-center">
+                                        <div class="post-thumb">
+                                            <a href="#"><img src="assets/images/blog/s1.jpg" alt="post-image"></a>
+                                        </div>
+                                        <div class="post-content">
+                                            <span class="post-date">07 December 2018</span>
+                                            <h6 class="post-title"><a href="#">Some Good Resources That are Available Students</a></h6>
+                                        </div>
+                                    </li><!-- post-item end -->
+                                    <li class="post-item d-flex align-items-center">
+                                        <div class="post-thumb">
+                                            <a href="#"><img src="assets/images/blog/s2.jpg" alt="post-image"></a>
+                                        </div>
+                                        <div class="post-content">
+                                            <span class="post-date">07 December 2018</span>
+                                            <h6 class="post-title"><a href="#">Some Good Resources That are Available Students</a></h6>
+                                        </div>
+                                    </li><!-- post-item end -->
+                                    <li class="post-item d-flex align-items-center">
+                                        <div class="post-thumb">
+                                            <a href="#"><img src="assets/images/blog/s3.jpg" alt="post-image"></a>
+                                        </div>
+                                        <div class="post-content">
+                                            <span class="post-date">07 December 2018</span>
+                                            <h6 class="post-title"><a href="#">Some Good Resources That are Available Students</a></h6>
+                                        </div>
+                                    </li><!-- post-item end -->
+                                    <li class="post-item d-flex align-items-center">
+                                        <div class="post-thumb">
+                                            <a href="#"><img src="assets/images/blog/s4.jpg" alt="post-image"></a>
+                                        </div>
+                                        <div class="post-content">
+                                            <span class="post-date">07 December 2018</span>
+                                            <h6 class="post-title"><a href="#">Some Good Resources That are Available Students</a></h6>
+                                        </div>
+                                    </li><!-- post-item end -->
+                                    <li class="post-item d-flex align-items-center">
+                                        <div class="post-thumb">
+                                            <a href="#"><img src="assets/images/blog/s5.jpg" alt="post-image"></a>
+                                        </div>
+                                        <div class="post-content">
+                                            <span class="post-date">07 December 2018</span>
+                                            <h6 class="post-title"><a href="#">Some Good Resources That are Available Students</a></h6>
+                                        </div>
+                                    </li><!-- post-item end -->
+                                </ul>
+                            </div><!-- widget end -->
+                        </div>
+                    </div><!-- sidebar-area end -->
+                </div>
+            </div>
+        </section>
+        <!-- blog-grid-section end -->
+        <!-- footer-section start -->
+        <?php include './footer.php'; ?>
+        <!-- footer-section end -->
+
+        <!-- scroll-to-top start -->
+        <div class="scroll-to-top">
+            <span class="scroll-icon">
+                <i class="fa fa-rocket" aria-hidden="true"></i>
+            </span>
+        </div>
+        <!-- scroll-to-top end -->
+
+        <!-- jquery js link -->
+        <script src="rs-plugin/js/pluginson3step.js" type="text/javascript"></script>
+        <!--<script src="js/jquery-3.3.1.min.js"></script>-->
+        <!-- bootstrap js link -->
+        <script src="rs-plugin/js/plugin-set.js" type="text/javascript"></script>
+        <script src="js/bootstrap.bundle.js"></script>
+        <script src="js/bootstrap.min.js"></script>
+        <!-- owl carousel js link -->
+        <script src="js/owl.carousel.min.js"></script>
+        <!-- lightcase js link -->
+        <script src="js/lightcase.js"></script>
+        <!-- waypoints js link -->
+        <script src="js/jquery.waypoints.min.js"></script>
+        <!-- countup js link -->
+        <script src="js/jquery.countup.min.js"></script>
+        <!-- circle-progress js link -->
+        <script src="js/circle-progress.min.js"></script>
+        <!-- countdown js link -->
+        <script src="js/jquery.countdown.js"></script>
+        <!-- goolg-map-activate js link -->
+        <script src="js/goolg-map-activate.js"></script>
+        <!-- main js link -->
+        <script src="js/main.js"></script>
+        <script src="rs-plugin/js/jquery.themepunch.tools.min.js"></script>
+        <script src="rs-plugin/js/jquery.themepunch.revolution.min.js"></script>
+        <script src="rs-plugin/js/custom.js" type="text/javascript"></script>
+        <script src="rs-plugin/js/on3step.js" type="text/javascript"></script>
+
+    </body>
+
+</html>
